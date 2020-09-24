1c1f9933885f4c25f3da3fc6d6c612bdedc90183
Geovani José <geovani-lol@hotmail.com>
2020-09-24 15:29:45 -0300

    Implement global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
-const App: React.FC = () => <h1>cockta.io</h1>
+import GlobalStyle from './styles/global'
+
+const App: React.FC = () => (
+  <>
+    <h1>Cockta.io</h1>
+    <GlobalStyle />
+  </>
+)
 
 export default App
