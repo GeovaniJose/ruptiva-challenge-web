9196c0736523fefedb85203bd8a30b4e620ba15f
Geovani José <geovani-lol@hotmail.com>
2020-09-24 20:14:53 -0300

    Implement SignUp page

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { FiArrowLeft } from 'react-icons/fi'
+
+import { Container, Content, AnimationContainer, Background } from './styles'
+
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Background />
+
+      <Content>
+        <AnimationContainer>
+          {/* <img src={logoImg} alt='GoBarber' /> */}
+          <h1>Cockta.io</h1>
+
+          <form onSubmit={() => {}}>
+            <h1>Faça seu cadastro</h1>
+
+            <input name='name' placeholder='Nome' />
+            <input
+              name='age'
+              type='number'
+              min='18'
+              max='130'
+              placeholder='Idade'
+            />
+            <input name='email' placeholder='E-mail' />
+            <input name='password' type='password' placeholder='Senha' />
+            <button type='submit'>Cadastrar</button>
+          </form>
+
+          <a href='/'>
+            <FiArrowLeft />
+            Voltar para login
+          </a>
+        </AnimationContainer>
+      </Content>
+    </Container>
+  )
+}
+
+export default SignUp
